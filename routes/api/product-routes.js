53958e7688e37f41fde12e353e81dec323f612ff
Robby Rish <robbyrish13@gmail.com>
2022-03-09 15:17:43 -0500

    GET product by id

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -1,5 +1,5 @@
 const router = require('express').Router();
-const { Product } = require('../../models');
+const { Product, Category } = require('../../models');
 
 router.get('/', (req, res) => {
     Product.findAll()
@@ -15,34 +15,23 @@ router.get('/:id', (req, res) => {
       where: {
         id: req.params.id
       },
-      attributes: [
-        'id',
-        'post_url',
-        'title',
-        'created_at',
-        [sequelize.literal('(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)'), 'vote_count']
-      ],
       include: [
         {
-          model: Comment,
-          attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
-          include: {
-            model: User,
-            attributes: ['username']
-          }
+          model: Category,
+          attributes: ['category_name']
         },
         {
-          model: User,
-          attributes: ['username']
+          model: Tag,
+          attributes: ['tag_name']
         }
       ]
     })
-      .then(dbCategoryData => {
-        if (!dbCategoryData) {
-          res.status(404).json({ message: 'No Category found with this id' });
+      .then(dbProductData => {
+        if (!dbProductData) {
+          res.status(404).json({ message: 'No product found with this id' });
           return;
         }
-        res.json(dbCategoryData);
+        res.json(dbProductData);
       })
       .catch(err => {
         console.log(err);
