d07a5262559d25210d48921e43f2fe9f88938f82
Robby Rish <robbyrish13@gmail.com>
2022-03-09 17:44:39 -0500

    hello

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -55,9 +55,6 @@ router.post('/', (req, res) => {
 router.put('/:id', (req, res) => {
     Category.update(
       {
-        category_name: req.body.category_name
-      },
-      {
         where: {
           id: req.params.id
         }
