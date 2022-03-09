fe555a286ef326715bcc9f2d5e44ba49420bf75e
Robby Rish <robbyrish13@gmail.com>
2022-03-09 12:41:51 -0500

    Port already in use

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -1,5 +1,11 @@
 const router = require('express').Router();
-const { Category } = require('../../models');
+const { Category, Product } = require('../../models');
+
+
+
+router.get('/ping', (req, res) => {
+  res.send('ping'); 
+})
 
 router.get('/', (req, res) => {
     Category.findAll()
@@ -11,33 +17,20 @@ router.get('/', (req, res) => {
 });
 
 router.get('/:id', (req, res) => {
+  res.send('hellol')
     Category.findOne({
       where: {
         id: req.params.id
       },
-      attributes: [
-        'id',
-        'post_url',
-        'title',
-        'created_at',
-        [sequelize.literal('(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)'), 'vote_count']
-      ],
       include: [
         {
-          model: Comment,
-          attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
-          include: {
-            model: User,
-            attributes: ['username']
-          }
-        },
-        {
-          model: User,
-          attributes: ['username']
+          model: Product,
+          attributes: ['product_name']
         }
       ]
     })
       .then(dbCategoryData => {
+        console.log(dbCategoryData); 
         if (!dbCategoryData) {
           res.status(404).json({ message: 'No Category found with this id' });
           return;
--- routes/api/index.js
+++ routes/api/index.js
@@ -7,4 +7,7 @@ router.use('/categories', categoryRoutes);
 router.use('/products', productRoutes);
 router.use('/tags', tagRoutes);
 
+
+
 module.exports = router;
+
